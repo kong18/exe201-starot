c414d1039ee8dd90a6f087019f705a5a81962f5b
kong18 <thanh2000cong20000@gmail.com>
2024-09-18 11:36:39 +0700

    fix

--- Exe.Starot.Api/obj/Debug/net8.0/Exe.Starot.Api.AssemblyInfo.cs
+++ Exe.Starot.Api/obj/Debug/net8.0/Exe.Starot.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exe.Starot.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae2e0e75536672845736b3221f5969f51714ade5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exe.Starot.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exe.Starot.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exe.Starot/Pagination/PagedResult.cs
+++ Exe.Starot/Pagination/PagedResult.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Exe.Starot.Application.Pagination
 {
-    internal class PagedResult
+    public class PagedResult<T>
     {
+        public PagedResult()
+        {
+            Data = null!;
+        }
+
+        public static PagedResult<T> Create(
+            int totalCount,
+            int pageCount,
+            int pageSize,
+            int pageNumber,
+            IEnumerable<T> data)
+        {
+            return new PagedResult<T>
+            {
+                TotalCount = totalCount,
+                PageCount = pageCount,
+                PageSize = pageSize,
+                PageNumber = pageNumber,
+                Data = data,
+            };
+        }
+
+        public int TotalCount { get; set; }
+
+        public int PageCount { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public IEnumerable<T> Data { get; set; }
+
     }
 }
