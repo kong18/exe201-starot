52bac1155bf01f8f8ce8635dfb5f76eb0869a814
kong18 <thanh2000cong20000@gmail.com>
2024-09-21 21:14:51 +0700

    a

--- Exe.Starot.Infrastructure/obj/Debug/net8.0/Exe.Starot.Infrastructure.AssemblyInfo.cs
+++ Exe.Starot.Infrastructure/obj/Debug/net8.0/Exe.Starot.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exe.Starot.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32283cf7d10f554cbb7e80eb833f2db4952d4d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exe.Starot.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exe.Starot.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
