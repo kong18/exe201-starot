0249432d207c13fc0a51967823798eb558fde034
kong18 <thanh2000cong20000@gmail.com>
2024-09-25 00:53:35 +0700

    create order

--- Exe.Starot/Order/OrderDetailDTO.cs
+++ Exe.Starot/Order/OrderDetailDTO.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using Exe.Starot.Application.Common.Mappings;
+using Exe.Starot.Domain.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,17 @@ using System.Threading.Tasks;
 
 namespace Exe.Starot.Application.Order
 {
-    internal class OrderDetailDTO
+    public class OrderDetailDTO : IMapFrom<OrderDetailEntity>
     {
+        public string ID {  get; set; }
+        public string ProductID {  get; set; }
+        public int Amount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Price { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<OrderDetailEntity, OrderDetailDTO>();
+        }
     }
 }
